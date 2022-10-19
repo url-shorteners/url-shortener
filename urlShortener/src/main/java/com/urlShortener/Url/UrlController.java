a8dbf3e8bf430c7d469ba114e52df0c178c4bd8e
eunyoung23 <eun970923@naver.com>
2022-10-19 18:59:45 +0900

    API 설계서 피드백 반영해서 수정

--- urlShortener/src/main/java/com/urlShortener/Url/UrlController.java
+++ urlShortener/src/main/java/com/urlShortener/Url/UrlController.java
@@ -1,32 +1,36 @@
 package com.urlShortener.Url;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "1.Url")
-@RequestMapping("/shortenUrl")
+@RequestMapping("/shorten-url")
 public class UrlController {
 
     @RequestMapping(path = "/create",method = RequestMethod.POST)
+    @ApiResponse(responseCode = "200",description = "요청에 성공하였습니다.")
+    @ApiResponse(responseCode = "400",description = "올바른 URL형식이 아닙니다.")
     public void createUrl(){
 
     }
 
-    @RequestMapping(path = "/urlChanged",method = RequestMethod.GET)
-    public void redirect(){
+    @RequestMapping(path = "/{key}",method = RequestMethod.GET)
+    @ApiResponse(responseCode = "301",description = "생성된 단축 URL입니다.")
+    public void redirect(@PathVariable long key){
 
     }
 
-    @RequestMapping(path = "/popularRequest",method = RequestMethod.GET)
+    @RequestMapping(path = "/top-urls",method = RequestMethod.GET)
+    @ApiResponse(responseCode = "200",description = "요청에 성공하였습니다.")
     public void popularRequest(){
 
     }
 
-    @RequestMapping(path = "/countRequest",method = RequestMethod.GET)
-    public void countRequest(){
+    @RequestMapping(path = "/keys/{key}/summary",method = RequestMethod.GET)
+    @ApiResponse(responseCode = "200",description = "요청에 성공하였습니다.")
+    public void countRequest(@PathVariable long key){
 
     }
 
